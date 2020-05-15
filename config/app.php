7338ae2325e15bed5879acb94786a18b90a1dbd0
Aruna <arunaseun@yahoo.com>
2020-05-15 11:42:55 +0100

    links for pages error not found fixed

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://tutohub.herokuapp.com/'),
+    'url' => env('APP_URL', 'https://tutohub.herokuapp.com/'),
 
     'asset_url' => env('ASSET_URL', null),
 
